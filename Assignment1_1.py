04a90b91e853ff4b7437adf1ca66f9977a136fc8
navneet090982-byte <navneet090982@gmail.com>
2026-01-11 02:54:32 +0530

    Added Python Assignment 1

---
+++ Assignment1_1.py
@@ -0,0 +1,24 @@
+'''Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+'''
+
+Number1 = int(input("Enter First Number:"))
+Number2 = int(input("Enter Second Number:"))
+# Addition
+Addition_A = Number1 + Number2
+print("Addition result is:", Addition_A)
+# Subtraction
+Subtraction_S = Number1 - Number2
+print("Subtraction result is:", Subtraction_S)
+# Multipication
+Multiply_M = Number1 * Number2
+print("Multipication result is:", Multiply_M)
+# Division
+division_D = Number1/Number2
+print("Division result is:", division_D)
